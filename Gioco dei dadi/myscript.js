b41c5a3526a9a8e681fe0394863249681f180171
Ghadeer <ghaora@gmail.com>
2021-12-14 00:34:43 +0200

    js changes

--- Gioco dei dadi/myscript.js
+++ Gioco dei dadi/myscript.js
@@ -5,15 +5,17 @@ let result = document.getElementById("result");
 document.getElementById("dado-user").innerHTML = dadoUser;
 document.getElementById("dado-pc").innerHTML = dadoPC;
 
-if (dadoUser > dadoPC) {
+if(dadoUser > dadoPC){
   result.innerHTML = "You won!";
   result.classList.add("won");
 }
-else if (dadoUser < dadoPC) {
+
+else if(dadoUser < dadoPC){
   document.getElementById("result").innerHTML = "You Lost!";
   result.classList.add("lose");
 }
-else {
+
+else{
   document.getElementById("result").innerHTML = "Draw!";
   result.classList.add("draw");
 }
